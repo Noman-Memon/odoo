8d8fd6d4c2c1d84d1a815a8a89ba77f6c0a7be5e
Noman-Memon <noman78686@gmail.com>
2023-06-20 10:52:57 +0500

    create specific search view and default search view
    create search views by fields, adding filters by set domain,  adding groups by using context, set default sesarches

--- addons_custom/tutorial_project/models/staff.py
+++ addons_custom/tutorial_project/models/staff.py
@@ -12,16 +12,16 @@ class RestStaff(models.Model):
     dob = fields.Date(string='Date_Of_Birth')
     mobile = fields.Char(string='Mobile_Number')
     email = fields.Char(string="Email")
-    gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
     country_id = fields.Many2one('res.country',
                                  string="Country")  # when we want to get 1 record from multiple records of other models or objects
     country_ids = fields.Many2many('res.country', string="Countries")
     country_code = fields.Char(string="Country_code",
                                related="country_id.code")  # if we want to get related data of any field of many2one object then...
     staff_line_ids = fields.One2many('rest.staff.lines', 'connecting_field', string="Staff Line")
-    sequence = fields.Integer(string="seq.")
-    status = fields.Selection([('active','Active'),('resigned','Resigned')], string="status", readonly=True, default='active')
-
+    sequence = fields.Integer(string="Seq.")
+    status = fields.Selection([('active', 'Active'), ('resigned', 'Resigned')], string="status", readonly=True,
+                              default='active')
 
     def new_function(self):
         print("Hello!.")
@@ -39,12 +39,14 @@ class RestStaff(models.Model):
                         'message': 'record has been deleted',
                     }
                 }
+
     def changestatus(self):
         for record in self:
             if record.status == 'active':
                 record.status = 'resigned'
             else:
                 record.status = 'active'
+
 
 class RestStaffLines(models.Model):
     _name = 'rest.staff.lines'
@@ -53,4 +55,4 @@ class RestStaffLines(models.Model):
     name = fields.Char(string="Name", size=50)
     connecting_field = fields.Many2one('rest.staff', string="staff ID")
     product_id = fields.Many2one('product.product', string="Products")
-    sequence = fields.Integer(string="seq.")
+    sequence = fields.Integer(string="Seq.")
