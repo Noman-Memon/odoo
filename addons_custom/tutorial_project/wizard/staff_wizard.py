cb15af631cd84e7810e8e2cc382b332cea23ff8e
Noman-Memon <noman78686@gmail.com>
2023-07-06 09:37:02 +0500

    update record data through wizard its normal fields, its mony2one field, its many2many field, its one2many field

--- addons_custom/tutorial_project/wizard/staff_wizard.py
+++ addons_custom/tutorial_project/wizard/staff_wizard.py
@@ -12,6 +12,15 @@ from odoo import models, fields
 #         <field name="view_id" ref="tutorial_project.rest_staff_wizard_view_form"/>
 
 # after this create action button where ever we want and define this wizard's xml file action_id with project name '
+
+# by-default its create two buttons save and discard
+# we can also create our own buttons for calling any function e.g.
+# <button name="update_function" string="Update Info" type="object"
+#                                 class="oe_highlight"/>
+# <button string="Cancel" special="cancel"--------(special attribute is used in wizard for cancel button defining function not required)
+#                                 class="btn_secondary"/>
+
+
 class RestStaffWizard(models.TransientModel):
     _name = 'rest.staff.wizard'
     _description = 'This wizard will update information of staff'
@@ -32,6 +41,45 @@ class RestStaffWizard(models.TransientModel):
     hand_salary = fields.Float(string="In Hand Salary")
     epf_efi = fields.Float(string="EPF+ESI")
     ctc_salary = fields.Float(string="CTC")
+
+    # how to update record through wizard
+
+    def update_function(self):
+        # 1st get id of current form record through context (active_id = self._context.get('active_id'))
+        active_id = self._context.get('active_id')
+        print("active_id---", active_id)
+        # 2nd make this id browsable (upd_var = self.env['rest.staff'].browse(active_id))
+        # now we get wizard fields data through self and update record fields through browsable_id that is upd_var.write method
+        upd_var = self.env['rest.staff'].browse(active_id)
+        print(upd_var)
+        # for update many2many fields data
+        country_list = []  # in this list we get country ids
+        for vals in self.country_ids:
+            country_list.append(vals.id)
+        # for update one2many fields data
+        list2 = []
+        for val2 in self.staff_line_ids:
+            list2.append((0, 0, {
+                'name': val2.name,
+                'product_id': val2.product_id.id,
+            }))
+
+        # if we want to first empty one2many fields data and then update new data then do this
+        upd_var.staff_line_ids = [(5, 0, 0)]
+        # 3rd create dictionary and update fields data
+        vals = {
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+            'dob': self.dob,
+            'mobile': self.mobile,
+            'email': self.email,
+            'country_id': self.country_id.id,  # for update many2one field
+            'country_ids': [(6, 0, country_list)],
+            'staff_line_ids': list2,
+
+        }
+        upd_var.write(vals)
 
 
 class RestStaffWizardLines(models.TransientModel):
